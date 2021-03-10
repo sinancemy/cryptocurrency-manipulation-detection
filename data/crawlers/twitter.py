c4e835d41ad080ab91a0e4d57dcedeeb56cb987f
utkn <39ph4f87a@gmail.com>
2021-03-10 20:06:16 +0300

    Ivir zivir

--- data/crawlers/twitter.py
+++ data/crawlers/twitter.py
@@ -3,37 +3,39 @@ import datetime
 from database.models import Post
 
 # We will open these when we carry this over to a server.
-#usernames = ["officialmcafee", "VitalikButerin", "SatoshiLite", "pmarca", "rogerkver", "aantonop", "ErikVoorhees", "NickSzabo4", "CryptoYoda1338", "bgarlinghouse", "WhalePanda", "cryptoSqueeze", "ZeusZissou", "Beastlyorion", "bitcoin_dad", "jebus911", "Sicarious", "CryptoMessiah", "APompliano", "nic__carter", "CarpeNoctom", "Melt_Dem", "100trillionUSD", "MessariCrypto", "TuurDemeester", "MartyBent", "elonmusk"]
-#coins = ["Bitcoin", "BTC", "btc", "Ethereum", "ETH", "eth", "Dogecoin", "DOGE", "doge", "Cardano", "ADA", "ada", "Chainlink", "LINK", "link", "Polkadot", "DOT", "dot", "Binance coin", "BNB", "bnb", "Ripple", "XRP", "xrp", "OMG Network", "OMG", "omg", "Litecoin", "LTC", "ltc", "Stellar", "XLM", "xlm", "Basic Attraction Token", "BAT", "bat", "Avalanche", "AVAX", "avax", "Ravencoin", "RVN", "rvn", "Maker", "MKR", "mkr", "Chiliz", "CHZ", "chz"]
+usernames = ["officialmcafee", "VitalikButerin", "SatoshiLite", "pmarca", "rogerkver", "aantonop", "ErikVoorhees", "NickSzabo4", "CryptoYoda1338", "bgarlinghouse", "WhalePanda", "cryptoSqueeze", "ZeusZissou", "Beastlyorion", "bitcoin_dad", "jebus911", "Sicarious", "CryptoMessiah", "APompliano", "nic__carter", "CarpeNoctom", "Melt_Dem", "100trillionUSD", "MessariCrypto", "TuurDemeester", "MartyBent", "elonmusk"]
+coins = ["Bitcoin", "BTC", "btc", "Ethereum", "ETH", "eth", "Dogecoin", "DOGE", "doge", "Cardano", "ADA", "ada", "Chainlink", "LINK", "link", "Polkadot", "DOT", "dot", "Binance coin", "BNB", "bnb", "Ripple", "XRP", "xrp", "OMG Network", "OMG", "omg", "Litecoin", "LTC", "ltc", "Stellar", "XLM", "xlm", "Basic Attraction Token", "BAT", "bat", "Avalanche", "AVAX", "avax", "Ravencoin", "RVN", "rvn", "Maker", "MKR", "mkr", "Chiliz", "CHZ", "chz"]
 
-usernames = ["elonmusk", "SatoshiLite"]
-coins = ["Bitcoin", "Doge"]
+#usernames = ["elonmusk", "SatoshiLite"]
+#coins = ["Bitcoin", "Doge"]
 
 
-def get_tweets():
+def get_tweets(coin):
     # TODO: Think about a good timer
     # threading.Timer(10.0, get_tweets).start()
     posts = []
-    for coin in coins:
-        for username in usernames:
-            tweets = []
-            c = twint.Config()
-            c.Limit = 1
-            c.Username = username
-            c.Search = coin
-            c.Store_object = True
-            c.Store_object_tweets_list = tweets
-            c.Hide_output = True
-            twint.run.Search(c)
-            for tweet in tweets:
-                tweet_id = tweet.id
-                username = tweet.username
-                tweet_body = tweet.tweet
-                interaction_score = calculate_interaction_score(tweet.replies_count, tweet.likes_count, tweet.retweets_count)
-                unix_timestamp = convert_to_unix(tweet.datestamp, tweet.timestamp)
-                comment_model = Post("tw"+str(tweet_id), username, tweet_body, interaction_score, "twitter", unix_timestamp)
-                posts.append(comment_model)
-
+    print("TwitterCrawler: Currently searching", coin)
+    for username in usernames:
+        print("TwitterCrawler: Current user @" + username)
+        tweets = []
+        c = twint.Config()
+        c.Limit = 1
+        c.Username = username
+        c.Search = coin
+        c.Store_object = True
+        c.Store_object_tweets_list = tweets
+        c.Hide_output = True
+        twint.run.Search(c)
+        for tweet in tweets:
+            tweet_id = tweet.id
+            username = tweet.username
+            tweet_body = tweet.tweet
+            interaction_score = calculate_interaction_score(tweet.replies_count, tweet.likes_count,
+                                                            tweet.retweets_count)
+            unix_timestamp = convert_to_unix(tweet.datestamp, tweet.timestamp)
+            comment_model = Post("tw" + str(tweet_id), username, tweet_body, interaction_score, "twitter",
+                                 unix_timestamp)
+            posts.append(comment_model)
     return posts
 
 def calculate_interaction_score(replies_count, likes_count, retweet_count):
--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from database.database import Database, recreate_database
 import data.crawlers.reddit as rd_crawler
+import data.crawlers.twitter as tw_crawler
 import data.market as mrk_crawler
 import time
 
 print("Recreating the database...")
-recreate_database()
+#recreate_database()
 
 print("Connecting to the database...")
 db = Database()
 
 print("Gathering from Reddit...")
-reddit_posts = rd_crawler.get_reddit_posts_as_models(limit=10)
+#reddit_posts = rd_crawler.get_reddit_posts_as_models(limit=10)
 print("Reddit posts gathered. Inserting into the database...")
-db.insert_posts(reddit_posts)
+#db.insert_posts(reddit_posts)
 print("Gathering from Twitter...")
-tw_crawler = None
-# TBD
+tweets = tw_crawler.get_tweets("BTC")
+print("Tweets gathered. Inserting into the database...")
+db.insert_posts(tweets)
 print("Gathering price information...")
 # Gather bitcoin price information for the last 1 hour with 1 minute intervals.
 price_history = mrk_crawler.pull_coin_prices_as_models("BTC", time.time(), time.time() - 60 * 60, "1m")
