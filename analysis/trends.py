bee5e9db6119f55579de79d72612fd9b36c7706a
Sierox <sinancem_yucel@hotmail.com>
2021-03-19 13:24:06 +0300

    Reorganized and documented trends.py

--- analysis/trends.py
+++ analysis/trends.py
@@ -1,7 +1,31 @@
 import pandas as pd
-from data.market.yahoo import *
 import matplotlib.pyplot as plt
+from data.market.yahoo import *
 from data.datacollector import *
+
+
+def analyze_trends(price_list):
+    """
+    Given a list of MarketPrice's, returns EMA8, SMA13, SMA21, and SMA55 slope values after a trend analysis.
+
+    :param price_list: A list of MarketPrice model objects, where the n/2-th price is the initial point of the
+    time interval at interest (e.g. price of the coin at the time of the post). Price data before and after the
+    n/2-th price are used for computing the averages, and a minimum of 55 days prior and 55 days posterior to it
+    should be included in the list.
+    :return: EMA8, SMA13, SMA21, SMA55 slopes of the given price_list centered at the middle of the list.
+    """
+    df = pd.DataFrame([price.price for price in price_list], index=[price.time for price in price_list])
+
+    ema8 = _slope(_exponential_moving_average(_select_time_range(df, _get_time_window_range(df, 8, False)), 8),
+                  _get_time_window_range(df, 8, True))
+    sma13 = _slope(_simple_moving_average(_select_time_range(df, _get_time_window_range(df, 13, False)), 13),
+                   _get_time_window_range(df, 13, True))
+    sma21 = _slope(_simple_moving_average(_select_time_range(df, _get_time_window_range(df, 21, False)), 21),
+                   _get_time_window_range(df, 21, True))
+    sma55 = _slope(_simple_moving_average(_select_time_range(df, _get_time_window_range(df, 55, False)), 55),
+                   _get_time_window_range(df, 55, True))
+
+    return ema8, sma13, sma21, sma55
 
 
 def _simple_moving_average(df, avg_time):
@@ -21,38 +45,38 @@ def _slope(df, time_range):
            / (time_range.high - time_range.low) * (df.index[1] - df.index[0])
 
 
-# TODO: Implement: 8EMA, 13SMA, 21SMA, 55SMA
-def analyze_trends(price_list):
-    coin = price_list[0].coin_type
-    df = pd.DataFrame([price.price for price in price_list], index=[price.time for price in price_list])
-
-    ema8 = _slope(_exponential_moving_average(moving_average_time(df, 8), 8), slope_time(df, 8))
-    sma13 = _slope(_simple_moving_average(moving_average_time(df, 13), 13), slope_time(df, 13))
-    sma21 = _slope(_simple_moving_average(moving_average_time(df, 21), 21), slope_time(df, 21))
-    sma55 = _slope(_simple_moving_average(moving_average_time(df, 55), 55), slope_time(df, 55))
-
-    return ema8, sma13, sma21, sma55
+def _select_time_range(df, time_range):
+    return df.loc[time_range.low:time_range.high]
 
 
-def moving_average_time(df, time_range):
-    start = round(((df.index[0] + df.index[-1])/2)/(60 * 60)) * (60 * 60) - time_range * 60 * 60 * 24
-    end = round(((df.index[0] + df.index[-1])/2)/(60 * 60)) * (60 * 60) + time_range * 60 * 60 * 24
-    return df.loc[start:end]
-
-
-def slope_time(df, time_range):
-    middle = round(((df.index[0] + df.index[-1])/2)/(60 * 60)) * (60 * 60)
-    end = middle + time_range * 60 * 60 * 24
-    return TimeRange(middle, end)
+def _get_time_window_range(df, time_window, start_from_middle=False):
+    start = round(((df.index[0] + df.index[-1]) / 2) / (60 * 60)) * (60 * 60) - time_window * 60 * 60 * 24 \
+        if not start_from_middle else round(((df.index[0] + df.index[-1]) / 2) / (60 * 60)) * (60 * 60)
+    end = round(((df.index[0] + df.index[-1]) / 2) / (60 * 60)) * (60 * 60) + time_window * 60 * 60 * 24
+    return TimeRange(start, end)
 
 
 def _example():
-    print(analyze_trends(pull_coin_history_as_models(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h")))
-    plt.plot(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h")[["Price"]])
+    """
+    An example analyze_trends call for reference and debugging purposes.
+    """
+    ema8, sma13, sma21, sma55 = analyze_trends(
+        pull_coin_history_as_models(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"))
+    print("EMA8  Slope: ", ema8)
+    print("SMA13 Slope: ", sma13)
+    print("SMA21 Slope: ", sma21)
+    print("SMA55 Slope: ", sma55)
+    plt.plot(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h")[["Price"]], label="BTC/USD")
     plt.plot(
-        _exponential_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 8 * 24))
-    plt.plot(_simple_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 13 * 24))
-    plt.plot(_simple_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 21 * 24))
-    plt.plot(_simple_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 55 * 24))
+        _exponential_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 8 * 24),
+        label="EMA8")
+    plt.plot(_simple_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 13 * 24),
+             label="SMA13")
+    plt.plot(_simple_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 21 * 24),
+             label="SMA21")
+    plt.plot(_simple_moving_average(pull_coin_history(CoinType.BTC, TimeRange(1577836800, 1587340800), "1h"), 55 * 24),
+             label="SMA55")
+    plt.legend(loc='lower left', frameon=False)
     plt.show()
+
 # _example()
