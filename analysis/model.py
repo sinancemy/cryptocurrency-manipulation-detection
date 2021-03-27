a391ef2c33da97ce0a098dd2074d5ce1ab53bf29
Sierox <sinancem_yucel@hotmail.com>
2021-03-27 21:16:29 +0300

    Implemented working model & training, except model gets stuck at bad local minima, needs rework.

--- analysis/model.py
+++ analysis/model.py
@@ -3,22 +3,37 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 
+class Swish(nn.Module):
+    def __init__(self, beta):
+        super(Swish, self).__init__()
+        self.beta = beta
+
+    def forward(self, x):
+        return x * torch.sigmoid(self.beta * x)
+
+
 class CryptoSpeculationModel(nn.Module):
     def __init__(self, device, vocab_size, embed_dim, lstm_hidden_dim, fc_dims, out_dim, batch_size, dropout=0.5):
         super().__init__()
+        self.device = device
         self.batch_size = batch_size
+        self.lstm_hidden_dim = lstm_hidden_dim
 
         self.embed = nn.Embedding(vocab_size, embed_dim)  # TODO: Initialize with pre-trained embeddings.
-        self.lstm = nn.LSTM(embed_dim, lstm_hidden_dim, bidirectional=True, dropout=dropout)
+        self.lstm = nn.LSTM(embed_dim, self.lstm_hidden_dim, bidirectional=True, dropout=dropout)
         fc_layers = []
         for i in range(0, len(fc_dims) - 1):
             fc_layers += [nn.Linear(fc_dims[i], fc_dims[i + 1]),
-                          nn.ReLU()]  # TODO: Try Swish!
+                          Swish(0.75)]  # TODO: Try Swish!
         self.fc = nn.Sequential(*fc_layers)
         self.out = nn.Linear(fc_dims[-1], out_dim)
 
-        self.lstm_hidden = Variable(torch.zeros(2, batch_size, lstm_hidden_dim).to(device))
-        self.lstm_cell = Variable(torch.zeros(2, batch_size, lstm_hidden_dim).to(device))
+        self.lstm_hidden = self.lstm_cell = None
+        self.reset_lstm_inputs()
+
+    def reset_lstm_inputs(self):
+        self.lstm_hidden = Variable(torch.zeros(2, self.batch_size, self.lstm_hidden_dim).to(self.device))
+        self.lstm_cell = Variable(torch.zeros(2, self.batch_size, self.lstm_hidden_dim).to(self.device))
 
     def forward(self, x_content, x_user, x_source, x_interaction):
         x_embed = self.embed(x_content).view(x_content.shape[1], self.batch_size, -1)
@@ -26,3 +41,7 @@ class CryptoSpeculationModel(nn.Module):
         x = torch.cat((torch.sigmoid(x_semantic.view(self.batch_size, -1)), x_user, x_source, x_interaction), dim=1)
         x = self.fc(x)
         return self.out(x)
+
+    def to(self, device):
+        super().to(device)
+        self.device = device
--- analysis/operate.py
+++ analysis/operate.py
@@ -14,22 +14,37 @@ def train(model, dataset, device, epochs, batch_size, lr):
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
-    optimizer = optim.SGD(model.parameters(), lr=lr)
+    optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.5, 0.99))
     loss_function = nn.MSELoss()
 
     model.to(device)
 
-    for epoch in tqdm(range(epochs), "Training Epoch"):
+    for epoch in tqdm(range(epochs)):
         model.train()
+        torch.cuda.empty_cache()
         for i, batch in enumerate(train_loader):
             content, user, source, interaction, impact = batch
 
+            model.zero_grad()
+            model.batch_size = content.shape[0]
+            model.reset_lstm_inputs()
+
             prediction = model(Variable(content).to(device), Variable(user).to(device),
                                Variable(source).to(device), Variable(interaction).to(device))
-            loss = loss_function(prediction, Variable(impact).to(device))
-            # print(loss)
 
-        model.eval()
+            true = Variable(impact).to(device)
+            loss = loss_function(prediction, true)
+            loss.backward()
+            optimizer.step()
+
+            # for p in zip(prediction, true):
+            #     print(p)
+
+        for p in zip(prediction, true):
+            print("Pred:", p[0])
+            print("True:", p[1])
+
+        # model.eval()
 
 
 if torch.cuda.is_available():
@@ -37,21 +52,22 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
-EPOCHS = 8
-BATCH_SIZE = 8
+torch.backends.cudnn.enabled = False
+
+EPOCHS = 16
+BATCH_SIZE = 1024
 LR = 2e-4
 
 dataset = CryptoSpeculationDataset("sample_set_2020_2021")
 
 VOCAB_SIZE = len(dataset.vectorizer.domains[0]) + 1  # TODO: Add <pad> token for padding sentences.
-EMBED_DIM = 32
-LSTM_HIDDEN_DIM = 64
+EMBED_DIM = 64
+LSTM_HIDDEN_DIM = 128
 FC_DIMS = [
-    (dataset.vectorizer.domains[0].max_sentence_length * EMBED_DIM * 4 + len(dataset.vectorizer.domains[1]) + len(
-        dataset.vectorizer.domains[2]) + 1), 1024, 512, 256]
+    (dataset.vectorizer.domains[0].max_sentence_length * LSTM_HIDDEN_DIM * 2 + len(dataset.vectorizer.domains[1]) + len(
+        dataset.vectorizer.domains[2]) + 1), 4096, 512, 128]
 OUT_DIM = 4
 
-model = CryptoSpeculationModel(device, VOCAB_SIZE, EMBED_DIM, LSTM_HIDDEN_DIM, FC_DIMS, OUT_DIM,
-                               BATCH_SIZE)
+model = CryptoSpeculationModel(device, VOCAB_SIZE, EMBED_DIM, LSTM_HIDDEN_DIM, FC_DIMS, OUT_DIM, BATCH_SIZE)
 
 train(model, dataset, device, EPOCHS, BATCH_SIZE, LR)
