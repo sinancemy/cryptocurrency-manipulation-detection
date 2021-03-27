Fixed inter-model shapes. Added loss calculation. Current model doesn't fit in GPU.

analysis/model.py
@@ -1,16 +1,15 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.autograd import Variable
 
 
 class CryptoSpeculationModel(nn.Module):
-    def __init__(self, vocab_size, embed_dim, lstm_hidden_dim, fc_dims, out_dim, batch_size, dropout=0.5):
-        super(self)
+    def __init__(self, device, vocab_size, embed_dim, lstm_hidden_dim, fc_dims, out_dim, batch_size, dropout=0.5):
+        super().__init__()
         self.batch_size = batch_size
 
         self.embed = nn.Embedding(vocab_size, embed_dim)  # TODO: Initialize with pre-trained embeddings.
-        self.lstm = nn.LSTM(embed_dim, lstm_hidden_dim, dropout=dropout)
+        self.lstm = nn.LSTM(embed_dim, lstm_hidden_dim, bidirectional=True, dropout=dropout)
         fc_layers = []
         for i in range(0, len(fc_dims) - 1):
             fc_layers += [nn.Linear(fc_dims[i], fc_dims[i + 1]),
@@ -18,11 +17,12 @@ class CryptoSpeculationModel(nn.Module):
         self.fc = nn.Sequential(*fc_layers)
         self.out = nn.Linear(fc_dims[-1], out_dim)
 
-        self.lstm_hidden = Variable(torch.zeros(1, batch_size, lstm_hidden_dim).cuda())  # TODO: Verify shape.
+        self.lstm_hidden = Variable(torch.zeros(2, batch_size, lstm_hidden_dim).to(device))
+        self.lstm_cell = Variable(torch.zeros(2, batch_size, lstm_hidden_dim).to(device))
 
     def forward(self, x_content, x_user, x_source, x_interaction):
-        x_semantic, self.lstm_hidden = self.lstm(self.embed(x_content).view(len(x_content), self.batch_size, -1),
-                                                 self.lstm_hidden)
-        x = torch.cat((F.sigmoid(x_semantic), x_user, x_source, x_interaction), dim=1)  # TODO: Maybe dim=0 ?
+        x_embed = self.embed(x_content).view(x_content.shape[1], self.batch_size, -1)
+        x_semantic, (self.lstm_hidden, self.lstm_cell) = self.lstm(x_embed, (self.lstm_hidden, self.lstm_cell))
+        x = torch.cat((torch.sigmoid(x_semantic.view(self.batch_size, -1)), x_user, x_source, x_interaction), dim=1)
         x = self.fc(x)
         return self.out(x)

analysis/operate.py
@@ -1,5 +1,7 @@
+import torch
 import torch.optim as optim
 import torch.nn as nn
+from tqdm import tqdm
 
 from data.dataset import CryptoSpeculationDataset
 from analysis.model import CryptoSpeculationModel
@@ -7,31 +9,49 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 
 
-def train(model, dataset, epochs, batch_size, lr):
+def train(model, dataset, device, epochs, batch_size, lr):
     train_set, test_set = dataset.partition(0.9)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
     optimizer = optim.SGD(model.parameters(), lr=lr)
-    loss_function = nn.CrossEntropyLoss()
+    loss_function = nn.MSELoss()
 
-    for epoch in range(epochs):
-        pass
+    model.to(device)
 
+    for epoch in tqdm(range(epochs), "Training Epoch"):
+        model.train()
+        for i, batch in enumerate(train_loader):
+            content, user, source, interaction, impact = batch
+
+            prediction = model(Variable(content).to(device), Variable(user).to(device),
+                               Variable(source).to(device), Variable(interaction).to(device))
+            loss = loss_function(prediction, Variable(impact).to(device))
+            # print(loss)
+
+        model.eval()
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
 
 EPOCHS = 8
-BATCH_SIZE = 64
+BATCH_SIZE = 8
 LR = 2e-4
 
 dataset = CryptoSpeculationDataset("sample_set_2020_2021")
 
-VOCAB_SIZE = len(dataset.vectorizer.v)
+VOCAB_SIZE = len(dataset.vectorizer.domains[0]) + 1  # TODO: Add <pad> token for padding sentences.
 EMBED_DIM = 32
 LSTM_HIDDEN_DIM = 64
-FC_DIMS = [(dataset.vectorizer.v.max_sentence_length + len(dataset.vectorizer.u) + len(dataset.vectorizer.s) + 1),
-           1024, 512, 256]
+FC_DIMS = [
+    (dataset.vectorizer.domains[0].max_sentence_length * EMBED_DIM * 4 + len(dataset.vectorizer.domains[1]) + len(
+        dataset.vectorizer.domains[2]) + 1), 1024, 512, 256]
 OUT_DIM = 4
 
-model = CryptoSpeculationModel(len(dataset.vectorizer.v), EMBED_DIM, LSTM_HIDDEN_DIM, FC_DIMS, OUT_DIM, BATCH_SIZE)
+model = CryptoSpeculationModel(device, VOCAB_SIZE, EMBED_DIM, LSTM_HIDDEN_DIM, FC_DIMS, OUT_DIM,
+                               BATCH_SIZE)
 
-train(model, dataset, EPOCHS, BATCH_SIZE, LR)
+train(model, dataset, device, EPOCHS, BATCH_SIZE, LR)

data/dataset.py
@@ -3,6 +3,7 @@ import random
 from pathlib import Path
 
 from torch.utils.data import Dataset
+from torch import ShortTensor, FloatTensor, IntTensor
 import numpy as np
 from tqdm import tqdm
 
@@ -62,8 +63,11 @@ class CryptoSpeculationDataset(Dataset):
     def __len__(self):
         return len(self.data_points)
 
-    def __getitem__(self, item):
-        return self.data_points[item]
+    def __getitem__(self, index):
+        item = self.data_points[index]
+        return (IntTensor(item.X.content), ShortTensor(item.X.user),
+                ShortTensor(item.X.source), ShortTensor([item.X.interaction]),
+                FloatTensor(item.y.impact))
 
     def __repr__(self):
         return "CryptoSpeculationDataset: %s\n" \