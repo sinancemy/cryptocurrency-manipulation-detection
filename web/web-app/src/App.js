fbf298115a70d6c8bb6440f46a30a17d816b915b
Hakan Şahin <hakansahin103098@gmail.com>
2021-04-01 18:00:57 +0300

    Added the center box part

--- web/web-app/src/App.js
+++ web/web-app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
 
 function App() {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 }
