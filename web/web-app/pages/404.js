6926d7e61883d230ae0b8235c07647b99b2f4293
Hakan Şahin <hakansahin103098@gmail.com>
2021-04-13 23:06:09 +0300

    Added a functioning 404 page

--- web/web-app/pages/404.js
+++ web/web-app/pages/404.js
@@ -1,3 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Custom404() {
-  return <h1>404</h1>;
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <Image src="/error.png" alt="Error 404" width="1470" height="598" />
+        <p className="text-white text-center text-5xl">
+          Sorry! The page you’re looking for cannot be found.{" "}
+          <text className="text-yellow-50 underline">
+            <Link className="" href="/">
+              Go to Home
+            </Link>
+          </text>
+        </p>
+      </div>
+    </div>
+  );
 }
