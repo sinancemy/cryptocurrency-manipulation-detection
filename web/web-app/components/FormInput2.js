727e0eb192f99dfd16c90465282046205e4d4fc8
tolga <tolgasumer1905@gmail.com>
2021-05-11 22:27:08 +0300

    forgot password page added to frontend and profile page updated

---
+++ web/web-app/components/FormInput2.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+
+export const FormInput2 = ({ label, placeholder, type, errorMsg, isDisabled = false, 
+                            checker = () => [true, null], setCorrectValue }) => {
+
+  const [value, setValue] = useState("")
+  const [firstUpdate, setFirstUpdate] = useState(false)
+  const [outsideError, setOutsideError] = useState(true)
+  const [innerErrorMsg, setInnerErrorMsg] = useState("")
+
+  useEffect(() => {
+    if(!firstUpdate || outsideError) return
+    const [res, err] = checker(value)
+    if(res) {
+      setInnerErrorMsg("")
+      setCorrectValue(value)
+    } else {
+      setInnerErrorMsg(err)
+      setCorrectValue(null)
+    }
+  })
+
+  useEffect(() => {
+    setOutsideError(true)
+    setInnerErrorMsg(errorMsg)
+  }, [errorMsg])
+
+  useEffect(() => {
+    setOutsideError(false)
+  }, [value])
+
+  return (
+    <div class="flex flex-col space-y-2">
+    <label className="block text-700 text-md font-bold mb-2">
+      { label }
+      <input
+        type={type}
+        placeholder={placeholder}
+        onFocus={() => setFirstUpdate(true)}
+        disabled={isDisabled}
+        value={value}
+        onChange={(e) => setValue(e.currentTarget.value)}
+        className={`mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+            ${(innerErrorMsg !== "") ? "border-red-500" : null}`} />
+    </label>
+    <p class="text-red-500 text-xs italic mt-2 ml-1">
+      {innerErrorMsg}
+    </p>
+  </div>
+  )
+}
--- web/web-app/pages/profile/index.js
+++ web/web-app/pages/profile/index.js
@@ -3,14 +3,41 @@ import { useRequireLogin, useUser } from "../../user-hook"
 import { FollowOverview } from "../../components/FollowOverview"
 import { TabbedView } from "../../components/TabbedView"
 import { SimpleMenu } from "../../components/SimpleMenu"
-import { useMemo, useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import { MenuPages } from "../../components/MenuPages"
+import { FormInput2 } from "../../components/FormInput2";
 
 export default function Profile() {
   useRequireLogin()
   const { username, followedCoins, followedSources, areCoinNotificationsOn, areSourceNotificationsOn } = useUser()
   const followedGroups = useMemo(() => followedSources.filter(f => f.target.startsWith("*@")), [followedSources])
   const followedUsers = useMemo(() => followedSources.filter(f => !f.target.startsWith("*@")), [followedSources])
+  const [oldPassword, setOldPassword] = useState(null)
+  const [correctPassword, setCorrectPassword] = useState(null)
+  const [correctReenteredPassword, setCorrectReenteredPassword] = useState(null)
+  const [isLoading, setLoading] = useState(false);
+  const [oldEmail, setOldEmail] = useState(null)
+  const [correctEmail, setCorrectEmail] = useState(null)
+  const [emailErrorMsg, setEmailErrorMsg] = useState("");
+
+  // Username field checker.
+  const simpleFieldChecker = useCallback((val) => {
+    if (val === "") {
+      return [false, "This field is required."]
+    } else if (val.length < 5) {
+      return [false, "Too short."]
+    }
+    return [true, null]
+  }, [])
+
+  const emailChecker = useCallback((val) => {
+    const re = /^\S+@\S+$/
+    const valid = re.test(val.toLowerCase())
+    return [valid, "Invalid e-mail."]
+  }, [])
+
+  const canSubmit = useMemo(() => correctPassword && correctReenteredPassword, 
+  [correctPassword, correctReenteredPassword])
 
   const [selectedPage, setSelectedPage] = useState(0)
   return (
@@ -80,7 +107,69 @@ export default function Profile() {
                 Account Settings
               </DashboardPanel.Header>
               <DashboardPanel.Body>
-                Nothing yet.
+              <TabbedView options={["Change Email", "Change Password"]}>
+                  <div>
+                  <FormInput2 type={"text"} 
+                     type={"text"} 
+                     label={"Enter Old E-mail"} 
+                     placeholder={"Your e-mail"} 
+                     errorMsg={emailErrorMsg}
+                     isDisabled={isLoading} 
+                     checker={emailChecker}
+                     setCorrectValue={setOldEmail}  />
+                    <FormInput2 type={"text"} 
+                      label={"Enter New E-mail"} 
+                      placeholder={"New e-mail"} 
+                      errorMsg={emailErrorMsg}
+                      isDisabled={isLoading} 
+                      checker={(val) => [val !== oldEmail, "Your new e-mail cannot be same as your old e-mail"]} 
+                      setCorrectValue={setCorrectEmail}  />
+                    <button
+                        type="submit"
+                        disabled={!canSubmit || isLoading}
+                        class="w-full bg-yellow-50 text-blue-50 h-10 py-2 px-4 text-center rounded disabled:opacity-50 hover:bg-yellow-500" >
+                        {isLoading ? (
+                        <AiOutlineLoading className={`animate-spin`} />
+                        ) : (
+                          "Change Email"
+                        )}
+                    </button>
+                    </div>
+                    <div>
+                    <FormInput2 type={"password"} 
+                      label={"Enter Old Password"} 
+                      placeholder={"Old password"} 
+                      errorMsg={""}
+                      isDisabled={isLoading} 
+                      checker={simpleFieldChecker} 
+                      setCorrectValue={setOldPassword}  />
+                    <FormInput2 type={"password"} 
+                      label={"Enter New Password"} 
+                      placeholder={"New password"} 
+                      errorMsg={""}
+                      isDisabled={isLoading} 
+                      checker={(val) => [val !== oldPassword, "Your new password cannot be same as your old password"]} 
+                      setCorrectValue={setCorrectPassword}  />
+                    <FormInput2 
+                        type={"password"} 
+                        label={"Password (again)"} 
+                        placeholder={"Re-enter your new password"}  
+                        errorMsg={""}
+                        isDisabled={isLoading} 
+                        checker={(val) => [val === correctPassword, "Passwords do not match"]} 
+                        setCorrectValue={setCorrectReenteredPassword}  />
+                    <button
+                        type="submit"
+                        disabled={!canSubmit || isLoading}
+                        class="w-full bg-yellow-50 text-blue-50 h-10 py-2 px-4 text-center rounded disabled:opacity-50 hover:bg-yellow-500" >
+                        {isLoading ? (
+                        <AiOutlineLoading className={`animate-spin`} />
+                        ) : (
+                          "Change Password"
+                        )}
+                    </button>
+                    </div>
+                  </TabbedView>
               </DashboardPanel.Body>
             </DashboardPanel>
           <DashboardPanel restrictedHeight={false} collapsable={false}>
@@ -88,7 +177,10 @@ export default function Profile() {
                 Delete Your Account
               </DashboardPanel.Header>
               <DashboardPanel.Body>
-                Nothing yet.
+                <div>Your account will be deleted permanently.</div>
+                <button className="bg-yellow-50 text-blue-50 py-2 px-4 my-5 rounded">
+                  Delete Account
+                </button>
               </DashboardPanel.Body>
             </DashboardPanel>
           </MenuPages>
